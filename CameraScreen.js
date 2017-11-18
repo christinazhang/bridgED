a19720fef6c28f407a8cabdf6a4c02573d803916
christinasz <christinaszhang@gmail.com>
2017-11-18 14:35:22 -0500

    Added ResultScreen and cleaned up structure

---
+++ CameraScreen.js
@@ -0,0 +1,113 @@
+import React, {Component} from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Camera, Permissions } from 'expo';
+
+let API_KEY = "***REMOVED***";
+
+export default class CameraScreen extends Component {
+  static navigationOptions = {
+    title: 'Take Photo'
+  }
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  };
+
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  processImage = async () => {
+    if (this.camera) {
+      let photo = await this.camera.takePictureAsync();
+      let photoURI = await photo.uri;
+      let formdata = new FormData();
+
+      formdata.append('image', {
+        uri: photoURI,
+        type: 'image/jpg',
+        name: 'image.jpg',
+      });
+
+      let url = 'https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=' + API_KEY + '&version=2016-05-20';
+      let data = await fetch(url, {
+          method: 'post',
+          headers: {
+            "Accept": "application/json",
+            "Accept-Language": "en",
+            "Content-Type": "multipart/form-data"
+          },
+          body: formdata
+        }).then(response => {
+          return response;
+        }).catch(err => {
+          console.log(err)
+        })
+      let rawjson = await data.json();
+      //console.log(rawjson.images[0]);
+      return rawjson.images[0].classifiers[0].classes;
+    }
+  }
+
+  snap = async () => {
+    let data = await this.processImage();
+    this.props.navigation.navigate('Result', {data: data})
+  }
+
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera
+            ref = {ref=>{this.camera = ref;}}
+            style={{ flex: 1 }}
+            type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type: this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                  });
+                }}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Flip{' '}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={this.snap}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Snap{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
+  }
+}
---
+++ ResultScreen.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import { Text, View} from 'react-native';
+
+// ResultScreen is given the IBM Watson result from the picture as an array in
+// the following format:
+
+/* Array [
+        Object {
+          "class": "ginger ale",
+          "score": 0.958,
+          "type_hierarchy": "/food/beverage/soft drink/ginger ale",
+        },
+        Object {
+          "class": "soft drink",
+          "score": 0.959,
+        },
+        Object {
+          "class": "beverage",
+          "score": 0.959,
+        },
+        Object {
+          "class": "food",
+          "score": 0.959,
+        },
+        Object {
+          "class": "bottle green color",
+          "score": 1,
+        },
+      ]
+*/
+
+// You can access the parameters: this.props.navigation.state.params.data
+// To keep it less verbose, see below
+
+export default class ResultScreen extends Component {
+  static navigationOptions = {
+    title: 'Translation Result'
+  }
+  render() {
+    const {state} = this.props.navigation;
+    return(
+      <View>
+        {state.params.data.map((obj, index) => (
+          <Text key={index}>{obj.class}, {obj.score}</Text>
+        ))}
+      </View>
+    )
+  }
+}
