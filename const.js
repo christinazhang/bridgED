99c9e4b853789f5eb4552c432d951b912a2a9e83
RunEMC <runlin.li@gmail.com>
2017-11-19 06:32:41 -0500

    Added all languages

--- const.js
+++ const.js
@@ -1,32 +1,96 @@
 export var speakDict = {
   'I speak English': 'en',
   '나는 한국말을한다': 'ko',
-  'أنا أتحدث العربية': 'ar'
+  'أنا أتحدث العربية': 'ar',
+  'Ich spreche Deutsch': 'de',
+  'Eu falo português': 'pt',
+  'Je parle français': 'fr',
+  'فوقي بيتكلم عربى.': 'arz',
+  'Parlo italiano': 'it',
+  'Hablo español': 'es',
+  '私は日本語を話す': 'ja'
 }
 
 export var chooseArray = {
-  en: [{lbl: 'I want to learn Korean', val: 'ko'},
-       {lbl: 'I want to learn Arabic', val: 'ar'}],
-  ko: [{lbl: '나는 영어를 배우고 싶다', val: 'en'},
-       {lbl: '나는 아랍어를 배우고 싶다', val: 'ar'}],
-  ar: [{lbl: 'أريد أن أتعلم اللغة الإنجليزية', val: 'en'},
-       {lbl: 'أريد أن أتعلم الكورية', val: 'ko'}]
+  en: [
+        {lbl: 'I want to learn Korean', val: 'ko'},
+        {lbl: 'I want to learn Arabic', val: 'ar'},
+        {lbl: 'I want to learn Spanish', val: 'es'},
+        {lbl: 'I want to learn German', val: 'de'},
+        {lbl: 'I want to learn Portugese', val: 'pt'},
+        {lbl: 'I want to learn French', val: 'fr'},
+        {lbl: 'I want to learn Egyption Arabic', val: 'arz'},
+        {lbl: 'I want to learn Italian', val: 'it'},
+        {lbl: 'I want to learn Japanese', val: 'ja'}
+  ],
+  de: [
+        {lbl: 'Ich möchte Englisch lernen', val: 'en'}
+  ],
+  pt: [
+        {lbl: 'Eu quero aprender inglês', val: 'en'}
+  ],
+  fr: [
+        {lbl: "Je veux apprendre l'anglais", val: 'en'},
+        {lbl: "je veux apprendre l'espagnol", val: 'es'}
+  ],
+  arz: [
+        {lbl: "نفسى اتعلم انجليزى.", val: 'en'}
+  ],
+  it: [
+        {lbl: "Voglio imparare l'inglese.", val: 'en'}
+  ],
+  ja: [
+        {lbl: "英語を習いたいです", val: 'en'}
+  ],
+  es: [
+        {lbl: 'Quiero aprender inglés', val: 'en'},
+        {lbl: 'Quiero aprender francés', val: 'fr'}
+  ],
+  ko: [
+        {lbl: '나는 영어를 배우고 싶다', val: 'en'},
+        {lbl: '나는 아랍어를 배우고 싶다', val: 'ar'}
+  ],
+  ar: [
+        {lbl: 'أريد أن أتعلم اللغة الإنجليزية', val: 'en'},
+  ]
   }
 
 export var speakArray = [
   {val: 'en', lbl: 'I speak English'},
+  {val: 'es', lbl: 'Hablo español'},
+  {val: 'ja', lbl: '私は日本語を話す'},
+  {val: 'ar', lbl: 'أنا أتحدث العربية'},
+  {val: 'de', lbl: 'Ich spreche Deutsch'},
+  {val: 'pt', lbl: 'Eu falo português'},
+  {val: 'fr', lbl: 'Je parle français'},
+  {val: 'arz', lbl: 'فوقي بيتكلم عربى.'},
+  {val: 'it', lbl: 'Parlo italiano'},
   {val: 'ko', lbl: '나는 한국말을한다'},
   {val: 'ar', lbl: 'أنا أتحدث العربية'}
 ]
 
 export var CameraTitle = {
   en: 'Take Photo',
+  de: 'Foto übernehmen',
+  pt: 'Tire uma foto',
+  fr: 'Tire uma foto',
+  arz: 'بالصور',
+  it: 'Prendi una foto',
+  es: 'Tomar foto',
+  ja: '写真を',
   ko: '사진을 찍다',
-  ar: 'تصوير'
+  ar: 'تصوير',
 }
 
 export var ResultTitle = {
   en: 'Translation Result',
+  de: 'Translation Ergebnisse',
+  pt: 'Resultados de Conversão',
+  fr: 'Les résultats de la traduction',
+  arz: 'نتيجة الترجمة',
+  it: 'Risultati conversione',
+  es: 'Conversión de resultados',
+  ja: '翻訳結果',
   ko: '번역 결과',
   ar: 'نتيجة الترجمة'
 }
