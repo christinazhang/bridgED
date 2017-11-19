a9aeb7f360a5dd70252b78441e8c645d0773b29b
christinasz <christinaszhang@gmail.com>
2017-11-19 03:34:18 -0500

    YAY WIKIPEDIA WORKS

--- ResultScreen.js
+++ ResultScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View, Image} from 'react-native';
+import {Text, ScrollView, View, Image, StyleSheet, Dimensions} from 'react-native';
 import {languagesDict, languagesArray} from './const.js'
 import {ResultTitle} from './const'
 
@@ -39,7 +39,7 @@ import {ResultTitle} from './const'
 export default class ResultScreen extends Component {
   constructor() {
     super();
-    this.state = {translation: '', img: ''}
+    this.state = {translation: '', img: '', blurb: ''}
   }
   static navigationOptions = ({navigation}) => (
     {
@@ -50,8 +50,8 @@ export default class ResultScreen extends Component {
   async componentWillMount() {
     const {params} = this.props.navigation.state;
     let t = params.data[0].class;
-    let url = 'https://gateway.watsonplatform.net/language-translator/api/v2/translate?source=' + params.inputLang + '&target=' + params.outputLang + '&text=' + t
-    let translation = await fetch(url, {
+    let translationURL = 'https://gateway.watsonplatform.net/language-translator/api/v2/translate?source=' + params.inputLang + '&target=' + params.outputLang + '&text=' + t
+    let translation = await fetch(translationURL, {
         method: 'get',
         headers: {
           'Authorization': 'Basic OTBhZDViNjgtZjU5YS00NzQxLTg2NGQtYmVlOGE0ZjYzZjcxOmxseW83aDdueEttNQ=='
@@ -62,35 +62,71 @@ export default class ResultScreen extends Component {
       }).catch(err => {
         console.log(err)
       })
-      console.log(translation);
-      this.setState({translation: translation});
-/*
-      let str = params.data[0].class;
-
-      let api = "AIzaSyBVVbxDT9RAFGv6gj-emTS7HD9lbWN50mQ";
-      url = "https://www.googleapis.com/customsearch/v1?key=" + api + "&cx=015596321862413156668:gxgkobpaaoq&q=" + str.replace("//g", "%20");
-      let data = await fetch(url);
-      let rawjson = await data.json();
-
-      console.log(rawjson);
-      this.setState({img:rawjson});*/
+    let delimited = t.split(' ').join('%20');
+    let blurbURL = 'https://' + params.inputLang + '.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&redirects=1&titles='
+    + delimited;
+    let blurbData = await fetch(blurbURL, {method: 'get'})
+      .then(response => (JSON.parse(response._bodyInit).query.pages))
+      .catch(err => {console.log(err)});
+    let id = Object.keys(blurbData)[0];
+    let blurb = blurbData[id].extract;
+    this.setState({blurb: blurb, translation: translation})
   }
 
   render() {
     const {params} = this.props.navigation.state;
 
-    let str = params.data[0].class;
+    let inputResult = params.data[0].class;
     let uriMe = params.uri;
 
     return(
-      <View>
-        <Text>{params.outputLang}  |  {params.inputLang}</Text>
-        <Text>{this.state.translation} | {str}</Text>
+      <ScrollView style={{flexDirection: 'column'}}>
+        <View style={{flexDirection: 'row',
+           backgroundColor: '#A9F7EB',
+           alignItems: 'center',
+         paddingTop: 10,
+       paddingBottom: 10}}>
+          <View style={{flexDirection: 'column',
+                      alignItems: 'flex-end',
+                      margin: 10,
+                      width: ((Dimensions.get('window').width - 30) / 3)}}>
+            <Text>{params.outputLang.toUpperCase()}</Text>
+            <Text style={styles.titleTextRight}>{this.state.translation}</Text>
+          </View>
+          <View style={{backgroundColor: '#222', width: 3,
+            height: 120}}></View>
+          <View style={{flexDirection: 'column',
+          width: ((Dimensions.get('window').width - 30) / 3)*2, margin: 10}}>
+            <Text>{params.inputLang.toUpperCase()}</Text>
+            <Text style={styles.titleText}>{inputResult}</Text>
+          </View>
+        </View>
         <Image
-          style={{width: 250, height: 250}}
+          style={styles.image}
           source={{uri:uriMe}}
         />
-      </View>
+        <Text style={{margin: 10}}>{this.state.blurb}</Text>
+      </ScrollView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  baseText: {
+    fontFamily: 'Cochin',
+  },
+  image: {
+    width: Dimensions.get('window').width - 50,
+    height: 200,
+    margin: 25
+  },
+  titleText: {
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
+  titleTextRight: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    textAlign: 'right'
+  },
+});
